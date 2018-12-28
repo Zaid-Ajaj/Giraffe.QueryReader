028ba38d3304d8b5fd4545b1df412e68b9d61a42
Zaid <zaid.naom@gmail.com>
2018-12-28 04:44:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,162 @@
+# Giraffe.QueryReader [![Build Status](https://travis-ci.org/Zaid-Ajaj/Giraffe.Giraffe.QueryReader.svg?branch=master)](https://travis-ci.org/Zaid-Ajaj/Giraffe.Giraffe.QueryReader) [![Nuget](https://img.shields.io/nuget/v/Giraffe.Giraffe.QueryReader.svg?colorB=green)](https://www.nuget.org/packages/Giraffe.Giraffe.QueryReader)
+
+HttpHandler for better working with query string paramters within [Giraffe](https://github.com/giraffe-fsharp/Giraffe) applications. Easily extract values from query string in a type-safe manner without defining intermediate types for model binding. The library can parse primitive values and their optional counter parts.  
+
+# Install
+```bash
+# using nuget client
+dotnet add package Giraffe.QueryReader
+# using Paket
+.paket/paket.exe add Giraffe.QueryReader --project path/to/Your.fsproj
+```
+
+The library code is actually only a single-file: `QueryReader.fs` so you can add it manually to your project and modify however you want. 
+
+# Usage
+
+Namespace `Giraffe.QueryReader` is opened in all examples below
+
+`Query.read` is the only function is this library, it is overloaded so takes a number of strings as the first parameters being the names of the parameters in the query string (for now up to 5 parameters! need more? PRs are welcome) and the last parameter is a function that transforms the parsed query string parameter into another HttpHandler
+
+### Basis use case: reading *required* parameter as string
+```fs
+GET 
+  >=> route "/sayHello"
+  >=> Query.read("to", sprintf "Hello %s" >> text)
+```
+Notice that because we used `%s` the type of the parameter was inferred to be `string`. This implies that `to` is a required query string parameter and if omitted, a bad request (400) response is returned to the client, i.e.
+```bash
+curl /sayHello?to=John 
+OK (200) "Hello John"
+
+curl /sayHello 
+Bad Request (400) '{ "message": "Required query string parameter to was not found" }'
+```
+
+### Optional query string parameters
+```fs
+GET 
+  >=> route "/greet"
+  >=> Query.read("name", 
+        function 
+        | Some name -> text (sprintf "Hello %s" name)
+        | None -> text "Hello World")
+```
+Since the input type now has been inferred to be `Option<string>` the query string parameter can be omitted:
+```bash
+curl /greet?name=John
+OK (200) "Hello John"
+
+curl /greet
+OK (200) "Hello World"
+```
+### Use multiple query string parameters
+```fs
+GET 
+  >=> route "/data"
+  >=> Query.read("fromDate", "toDate", "format", 
+        fun fromDate toDate format -> 
+            let data = sprintf "Data from %s to %s as %s" fromDate toDate format
+            text data)
+```
+Notice here we are not reading the values as `DateTime` but using `string` instead. This library doesn't try to do everything and for parsing complex structures like `DateTime` or similar, just read as string and parse/validate yourself. 
+
+### Use *optional* boolean flags
+Mix and match different parameters of different types
+```fs
+GET 
+  >=> route "/ablums"
+  >=> Query.read("search", "includeArtist", 
+        fun search includeArtist -> 
+            match includeArtist with
+            | Some true -> text (sprintf "Search '%s' and include artist" search)
+            | Some false -> text (sprintf "Search '%s' and do not include artist" search)
+            | None -> text (sprintf "Just searching '%s', includeArtist omitted")
+        )           
+```
+Here we are using multiple query string parameters: `search` and `includeArtist` where the latter is an optional boolean flag (implied by the `Option<bool>` type):
+```bash
+# implicit includeArtist=true because flag is present 
+curl /albums?search=Metallica&includeArtist
+OK (200) "Search 'Metallica' and include artist"
+
+# explicit boolean value
+curl /albums?search=Metallica&includeArtist=true
+OK (200) "Search 'Metallica' and include artist"
+
+curl /albums?search=Metallica&includeArtist=false
+OK (200) "Search 'Metallica' and do not include artist"
+
+curl /albums?search=Metallica
+OK (200) "Just searching 'Metallica', includeArtist omitted"
+```
+
+### Parsing integers and floating numbers
+```fs
+GET 
+  >=> route "/int-sum"
+  >=> Query.read("x", "y", 
+        fun x y -> 
+            let sum = x + y
+            let result = sprintf "x + y = %d" sum
+            text result)
+```
+Here the parameters are inferred to be of type `int` because of using `+` and `%d`.
+```bash
+curl /int-sum?x=10&y=5 
+OK (200) "x + y = 15"
+```
+if you want to use `float` then just give a hint at the compiler:
+```fs
+GET 
+  >=> route "/number-sum"
+  >=> Query.read("x", "y", 
+        fun (x:float) y -> 
+            let sum = x + y
+            let result = sprintf "x + y = %.1f" sum
+            text result)
+``` 
+then use 
+```bash
+curl /number-sum?x=1.5&y=2.5
+OK (200) "x + y = 4.0"
+```
+Notice here that numbers are parsed with an `CultureInfo.InvariantCulture` format provider. If you want to use a different format provider, then read the query string parameter as string then parse yourself. 
+
+### Supported types
+ - `string` 
+ - `int`
+ - `bool`
+ - `float`
+ - `Option<string>`
+ - `Option<int>`
+ - `Option<bool>`
+ - `Option<float>`
+
+Using optional types implies that the query string parameter can be omitted, in which case, `None` will be passed to the function. 
+
+## Builds
+
+![Build History](https://buildstats.info/travisci/chart/Zaid-Ajaj/Giraffe.QueryReader)
+
+
+### Building
+
+
+Make sure the following **requirements** are installed in your system:
+
+* [dotnet SDK](https://www.microsoft.com/net/download/core) 2.0 or higher
+* [Mono](http://www.mono-project.com/) if you're on Linux or macOS.
+
+```
+> build.cmd // on windows
+$ ./build.sh  // on unix
+```
+
+### Watch Tests
+
+The `WatchTests` target will use [dotnet-watch](https://github.com/aspnet/Docs/blob/master/aspnetcore/tutorials/dotnet-watch.md) to watch for changes in your lib or tests and re-run your tests on all `TargetFrameworks`
+
+```
+./build.sh WatchTests
+```
---
+++ tests/Giraffe.QueryReader.Tests/Tests.fs
@@ -0,0 +1,246 @@
+module Tests
+
+open Expecto
+open Giraffe
+open Giraffe.QueryReader
+open System
+open System.IO
+open System.Linq
+open System.Net.Http
+open System.Collections.Generic
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.TestHost
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open System.Net
+
+let testWebApp : HttpHandler =
+  choose [ 
+    GET >=> route "/" >=> text "Index"
+    
+    // required parameter as string
+    route "/sayHello" >=> Query.read("to", sprintf "Hello %s" >> text) 
+    
+    // optional parameter as string
+    route "/greet" >=> Query.read("name", 
+      function 
+      | Some name -> text (sprintf "Hello %s" name)
+      | None -> text "Hello World")
+
+    // required query parameters as integers
+    route "/sum" >=> Query.read("a", "b", 
+      fun a b -> 
+        let sum = a + b
+        text (sprintf "a + b = %d" sum))
+
+    // optional flags
+    route "/logical" >=> Query.read("value", 
+      function 
+      | Some true -> text "value is true"
+      | Some false -> text "value is false"
+      | None -> text "value is omitted")
+
+    // required boolean flags
+    route "/required-boolean" >=> Query.read("value", 
+      function 
+      | true -> text "true"
+      | false -> text "false")
+
+    // max and match optional numbers
+    route "/numbers" >=> Query.read("x", "y", 
+      fun (x: float) (y: Option<float>) -> 
+        let sum = x + defaultArg y 0.0
+        text (sprintf "x + y = %.1f" sum))
+
+    setStatusCode 404 >=> text "Not Found" 
+  ]
+
+let pass() = Expect.isTrue true "Passed"
+let fail() = Expect.isTrue false "Failed"
+
+let rnd = System.Random()
+
+let appBuilder (app: IApplicationBuilder) = 
+  app.UseGiraffe testWebApp 
+
+let configureServices (services: IServiceCollection) = 
+  services.AddGiraffe()
+  |> ignore
+
+let createHost() =
+    WebHostBuilder()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .Configure(Action<IApplicationBuilder> appBuilder)
+        .ConfigureServices(Action<IServiceCollection> configureServices)
+
+let runTask task =
+    task
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+let httpGet (path : string) (client : HttpClient) =
+    path
+    |> client.GetAsync
+    |> runTask
+
+let isStatus (code : HttpStatusCode) (response : HttpResponseMessage) =
+    Expect.equal response.StatusCode code "Status code is wrong"
+    response
+
+let ensureSuccess (response : HttpResponseMessage) =
+    if not response.IsSuccessStatusCode
+    then response.Content.ReadAsStringAsync() |> runTask |> failwithf "%A"
+    else response
+
+let readText (response : HttpResponseMessage) =
+    response.Content.ReadAsStringAsync()
+    |> runTask
+    
+let readTextEqual content (response : HttpResponseMessage) =
+    response.Content.ReadAsStringAsync()
+    |> runTask
+    |> fun result -> Expect.equal result content "The expected and actual response content are not equal"
+
+[<Tests>]
+let tests =
+  testList "Giraffe.QueryReader" [
+
+    testCase "Root path / returns 'Index' as text" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "Index"
+
+    testCase "Unknown path returns status 404 not found " <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/non-existent-path"
+      |> isStatus HttpStatusCode.NotFound
+      |> readTextEqual "Not Found"
+
+    testCase "Basic use-case: required parameter as string" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/sayHello?to=Zaid"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "Hello Zaid"    
+
+      client
+      |> httpGet "/sayHello"
+      |> isStatus HttpStatusCode.BadRequest
+      |> ignore  
+
+    testCase "Basic use case: optional query parameter as string" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/greet"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "Hello World"
+
+      client 
+      |> httpGet "/greet?name=Zaid"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "Hello Zaid"
+
+    testCase "Required int query parameters can be parsed" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/sum?a=10&b=5"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "a + b = 15"
+
+    testCase "Required negative int query parameters can be parsed" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/sum?a=-10&b=-5"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "a + b = -15" 
+
+    testCase "Omitting one required int query parameters will shortcircuit as bad request" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/sum?b=5"
+      |> isStatus HttpStatusCode.BadRequest
+      |> ignore
+
+    testCase "Required boolean parameters cannot be omitted: otherwise returns bad request" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/required-boolean"
+      |> isStatus HttpStatusCode.BadRequest
+      |> ignore
+
+    testCase "Required boolean parameters cannot be omitted: returns OK when value exists" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/required-boolean?value"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "true"
+
+      client
+      |> httpGet "/required-boolean?value=true"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "true"
+
+      client
+      |> httpGet "/required-boolean?value=false"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "false"
+
+    testCase "Optional numbers" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/numbers?x=1.5&y=2.5"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "x + y = 4.0" 
+
+      client
+      |> httpGet "/numbers?x=1.5"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "x + y = 1.5" 
+
+    testCase "Optional boolean flags" <| fun _ ->
+      use server = new TestServer(createHost())
+      use client = server.CreateClient()
+
+      client
+      |> httpGet "/logical"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "value is omitted"
+
+      client
+      |> httpGet "/logical?value"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "value is true"    
+
+      client
+      |> httpGet "/logical?value=true" // explicit
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "value is true"          
+
+      client
+      |> httpGet "/logical?value=false"
+      |> isStatus HttpStatusCode.OK
+      |> readTextEqual "value is false"    
+  ]
